2e2b9ecfe7d552d6e4d331c1bdac7a8effefff51
Elide <zavalavinagreelide@gmail.com>
2023-05-21 13:11:28 -0600

    routers

--- server/routes/openai.js
+++ server/routes/openai.js
@@ -5,3 +5,23 @@ import { openai } from '../index.js';
 
 dotenv.config();
 const router = express.Router();
+
+router.post("/text", async (req, res) => {
+    try {
+        const  { text, activeChatId } = req.body;
+        console.log("text: ", text);
+        res.status(200).json({
+            message: "Text received",
+            text
+        });
+    } catch (error) {
+        console.log("error",error);
+        res.status(500).json({
+            message: "Internal server error",
+            error: error.message
+        })
+    }   
+});
+
+export default router;
+
